c544928ad3898f4d58c1c7785565d4c821d11cf0
한민정 <zoangrak@gmail.com>
2023-10-31 20:40:02 +0900

    Initialize

---
+++ src/main/java/com/projectx/dododoc/DododocApplication.java
@@ -0,0 +1,13 @@
+package com.projectx.dododoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DododocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DododocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectx/dododoc/DododocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectx.dododoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DododocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
